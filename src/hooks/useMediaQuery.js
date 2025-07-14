b9463c730b7b2d5f26f4a8ab4a9c6e525cd87c49
siantje <90453386+pharrahlita@users.noreply.github.com>
2025-07-14 20:02:49 +0100

    aAAAAGH
    helup

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    setMatches(media.matches);
+
+    const listener = (e) => setMatches(e.matches);
+    media.addEventListener('change', listener);
+
+    return () => media.removeEventListener('change', listener);
+  }, [query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
